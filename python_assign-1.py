4d37aa03f7d30e395acb0d6197ee25b1fc93bb62
Sesughter01 <charles.avul@gmail.com>
2022-01-21 18:17:54 +0100

    correction

--- python_assign-1.py
+++ python_assign-1.py
@@ -8,9 +8,9 @@ print(new_str)
 # 2. Change the value of True in this list to False. Print the result on the terminal
     # new_list = ['this', "brown", 55, "oxen", True, 0.85]
 
-# new_list = ['this', "brown", 55, "oxen", True, 0.85]
-# new_list[4] = False
-# print(new_list)
+new_list = ['this', "brown", 55, "oxen", True, 0.85]
+new_list[4] = False
+print(new_list)
 
 # 3. Write a Python program to print a specified list after removing the 0th, 4th and 5th elements. Sample List = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
 # Expected Output = ['Green', 'White', 'Black']
